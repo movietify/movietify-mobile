a3cc010319c1e9939c2a7e645f23baa9c0d30bcd
handeebrar <hande_ebrar@hotmail.com>
2019-03-01 13:13:33 +0300

    ...

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//Handle incoming POST requests to /auth
 router.post('/signup', (req, res, next) => {
     res.status(200).json({
         message: 'Sign up page.'
--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bod
+
 const authRoutes = require('./api/routes/auth');
 const movieRoutes = require('./api/routes/movie');
 const profileRoutes = require('./api/routes/profile');
 
+app.use(morgan('dev'));
+
+// Routes which should handle request
 app.use('/auth', authRoutes);
 app.use('/movie', movieRoutes);
 app.use('/profile', profileRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
