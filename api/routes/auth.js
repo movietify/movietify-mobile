8ca391a129bff62c02332a6ad3c038dd4436aa95
handeebrar <hande_ebrar@hotmail.com>
2019-03-04 23:00:13 +0300

    The user password was encrypted with bcrypt, and the user was denied registration with the same name or mail address.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,28 +1,49 @@
 const express = require('express');
 const router  = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const User    = require('../models/user');
 
 //Handle incoming POST requests to /auth
 router.post('/signup', (req, res, next) => {
-
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        name: req.body.name,
-        picture: req.body.picture,
-        city: req.body.city,
-        country: req.body.country
-    });
-
-    user.save().then(result => {
-        console.log(result);
+    User.find({email: req.body.email , username: req.body.username})
+    .exec()
+    .then(user => {
+        if(user.length >= 1) {
+            return res.status(409).json({
+                message: 'Mail or username exist'
+            });
+        } else {
+            bcrypt.hash(req.body.password,10,(err, hash) => {
+                if(err) {
+                    return res.status(500).json({
+                        error: err
+                    });
+                } else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: hash
+                    });
+                    user
+                        .save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                message: 'User created'
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+                    }   
+                });
+        }
     })
-    .catch(err => console.log(err));
-
-    res.end(JSON.stringify(req.body));
 });
 
 router.post('/signin', (req, res, next) => {
