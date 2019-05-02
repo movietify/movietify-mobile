56e5cd258e40cd74f3c95d983bf27c92a198a13a
handeebrar <hande_ebrar@hotmail.com>
2019-05-02 15:56:36 +0300

    Updated list model.

--- api/models/list.js
+++ api/models/list.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const listSchema = mongoose.Schema ({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    user_id: mongoose.Schema.Types.ObjectId
+    user_id: mongoose.Schema.Types.ObjectId,
+    movies_ids: Array,
+    followers_ids: Array
 });
 
 module.exports = mongoose.model('List', listSchema);
